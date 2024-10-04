be177d55c8bd3f2724640c83da6e20ade5a16cf0
Maresias <ale.maresias@gmail.com>
2024-10-04 01:07:55 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello words !'
+  return 'Hello words !'
 })
 
-
-
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then( () => {
-    console.log("Helo World !")
-})
+  })
+  .then(() => {
+    console.log('Helo World !')
+  })
