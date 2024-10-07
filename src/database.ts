585a507557062f81715ef0c30499ec6d2a00342c
Maresias <ale.maresias@gmail.com>
2024-10-06 21:48:23 -0300

    Variaveis de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as setupKenx, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DTABASE_URL not found')
+}
+
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from 'knex'
+import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'transação de teste',
-      amout: 1000,
-    })
-    .returning('*')
+  const transaction = await knex('transactions').select('*')
 
   return transaction
 })
